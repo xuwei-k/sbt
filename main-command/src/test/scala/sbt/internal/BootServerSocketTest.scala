dddd47f1da89e919a5d22a8bead486d51d2f285a
xuwei-k <6b656e6a69@gmail.com>
2025-09-28 18:42:37 +0900

    wip

--- main-command/src/test/scala/sbt/internal/BootServerSocketTest.scala
+++ main-command/src/test/scala/sbt/internal/BootServerSocketTest.scala
@@ -7,10 +7,6 @@ import java.nio.ByteBuffer
 import java.nio.channels.SocketChannel
 import java.nio.file.Files
 import java.nio.file.Paths
-import scala.concurrent.Await
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.*
 import scala.util.Random
 
 class BootServerSocketTest extends AnyFreeSpec {
@@ -34,12 +30,7 @@ class BootServerSocketTest extends AnyFreeSpec {
           .open(StandardProtocolFamily.UNIX)
         assert(client.connect(java.net.UnixDomainSocketAddress.of(path)))
 
-        val server = Await.result(
-          Future {
-            serverSocket.accept()
-          },
-          5.seconds
-        )
+        val server = serverSocket.accept()
 
         action(server, client)
       } finally {
@@ -57,45 +48,34 @@ class BootServerSocketTest extends AnyFreeSpec {
         Random.shuffle((Byte.MinValue to Byte.MaxValue).toList.map(_.toByte))
 
       "server to client" in withServerAndClient { (server, client) =>
-        Future {
-          values.foreach(x => server.write(x))
-          server.close()
-        }
+        values.foreach(x => server.write(x))
+        server.close()
 
-        val clientReadResult = Await.result(
-          Future {
-            Iterator
-              .continually {
-                val buf = ByteBuffer.allocate(1)
-                val x = client.read(buf)
-                x -> buf.array().head
-              }
-              .takeWhile(_._1 != -1)
-              .map(_._2)
-              .toList
-          },
-          5.seconds
-        )
+        val clientReadResult =
+          Iterator
+            .continually {
+              val buf = ByteBuffer.allocate(1)
+              val x = client.read(buf)
+              x -> buf.array().head
+            }
+            .takeWhile(_._1 != -1)
+            .map(_._2)
+            .toList
+
         assert(clientReadResult == values)
       }
 
       "client to server" in withServerAndClient { (server, client) =>
-        Future {
-          client.write(ByteBuffer.wrap(values.toArray))
-          client.close()
-        }
+        client.write(ByteBuffer.wrap(values.toArray))
+        client.close()
 
-        val result = Await.result(
-          Future {
-            Iterator
-              .continually(
-                server.read()
-              )
-              .takeWhile(_ != -1)
-              .toList
-          },
-          5.seconds
-        )
+        val result = Iterator
+          .continually(
+            server.read()
+          )
+          .takeWhile(_ != -1)
+          .toList
+
         assert(result == values.map(_ & 0xff))
       }
     }
