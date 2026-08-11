f724ac2949c33bfab96c4fab6b6790b82092dfdc
xuwei-k <6b656e6a69@gmail.com>
2026-08-11 17:26:29 +0900

    [skip ci]

--- buildfile/src/test/scala/sbt/internal/EvaluateConfigurationsSpec.scala
+++ buildfile/src/test/scala/sbt/internal/EvaluateConfigurationsSpec.scala
@@ -1,46 +1,63 @@
 package sbt.internal
 
 import sbt.internal.parser.AbstractSpec
-import sbt.internal.util.MessageOnlyException
 import sbt.internal.inc.PlainVirtualFile
 import sbt.io.IO
+
 import java.nio.file.Paths
+import java.nio.file.Path
 import java.io.File
+import java.net.URLClassLoader
 
 object EvaluateConfigurationsSpec extends AbstractSpec {
+  private def getCclasspath(): Seq[Path] = {
+    def loop(loader: ClassLoader, acc: List[Path]): Seq[Path] = {
+      println(loader.getClass.getName)
+      loader match {
+        case x: URLClassLoader if x.getClass.getName != "sbt.internal.ScalaLibraryClassLoader" =>
+          loop(
+            loader.getParent,
+            x.getURLs
+              .map(_.toURI)
+              .map(Paths.get)
+              .toList ::: acc
+          )
+        case _ =>
+          acc.distinct
+      }
+    }
+    loop(this.getClass.getClassLoader, Nil)
+  }
+
+  val imports = List(
+    ".sbt.*",
+    ".sbt.given",
+    ".sbt.BareBuildSyntax.*",
+    ".sbt.Keys.*",
+    ".sbt.nio.Keys.*",
+    ".sbt.util.CacheImplicits.given",
+    ".sbt.librarymanagement.LibraryManagementCodec.given",
+  ).map("import _root_." + _)
+
   test("Specific error message for defining types") {
     // https://github.com/sbt/sbt/issues/9566
 
-    val classpath = this.getClass.getClassLoader
-      .asInstanceOf[java.net.URLClassLoader]
-      .getURLs
-      .map(_.toURI)
-      .map(Paths.get)
-      .toList
+    val classpath = getCclasspath()
 
     IO.withTemporaryDirectory { tmpDir =>
       val sbtFile = new File(tmpDir, "my-build.sbt")
       IO.write(
         sbtFile,
-        """|name := "foo"
+        """|sbt.Keys.name := "foo"
            |
-           |class A
            |""".stripMargin
       )
-      try {
-        EvaluateConfigurations.evaluateConfiguration(
-          new Eval(Nil, classpath, None, None),
-          PlainVirtualFile(sbtFile.toPath),
-          Nil
-        )
-        fail()
-      } catch {
-        case error: MessageOnlyException =>
-          assert(
-            error.toString ==
-              "my-build.sbt:3: Defining types in *.sbt file is not supported"
-          )
-      }
+      val res = EvaluateConfigurations.evaluateConfiguration(
+        new Eval(Nil, classpath, None, None),
+        PlainVirtualFile(sbtFile.toPath),
+        imports
+      )
+      println(res)
     }
   }
 }
